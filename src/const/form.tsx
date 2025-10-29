3d23673acf3f8207a682b98560da16beb9120e1c
william <garlic.william@gmail.com>
2025-10-29 23:17:38 +0800

    update

--- src/const/form.tsx
+++ src/const/form.tsx
@@ -1,48 +1,25 @@
-import { ChainLabel } from "../components/chains/ChainLabel.tsx";
-import { TokenLabel } from "../components/chains/TokenLabel.tsx";
-import { CHAIN_ICON } from "./chain-rpc.ts";
+import { ChainLabel } from '../components/chains/ChainLabel.tsx';
+import { TokenLabel } from '../components/chains/TokenLabel.tsx';
+import { CHAIN_ICON } from './chain-rpc.ts';
 
 export const CHAIN_SELECT_OPTIONS = [
   {
-    label: (
-      <ChainLabel chainId={"1"} icon={CHAIN_ICON["1"]} name={"Ethereum"} />
-    ),
-    value: "1",
-  },
-  {
-    label: (
-      <ChainLabel chainId={"56"} icon={CHAIN_ICON["56"]} name={"BNB Chain"} />
-    ),
-    value: "56",
+    label: <ChainLabel chainId={'1'} icon={CHAIN_ICON['1']} name={'Ethereum'} />,
+    value: '1',
   },
 ];
 
 export const ASSETS_SELECT_OPTIONS = [
   {
-    label: (
-      <TokenLabel
-        symbol={"USDT"}
-        icon={"https://static.stakestone.io/assets/imgs/tokens/usdt1.svg"}
-      />
-    ),
-    value: "USDT",
+    label: <TokenLabel symbol={'USDT'} icon={'https://static.stakestone.io/assets/imgs/tokens/usdt1.svg'} />,
+    value: 'USDT',
   },
   {
-    label: (
-      <TokenLabel
-        symbol={"USDC"}
-        icon={"https://static.stakestone.io/assets/imgs/tokens/usdc2.svg"}
-      />
-    ),
-    value: "USDC",
+    label: <TokenLabel symbol={'USDC'} icon={'https://static.stakestone.io/assets/imgs/tokens/usdc2.svg'} />,
+    value: 'USDC',
   },
   {
-    label: (
-      <TokenLabel
-        symbol={"USD1"}
-        icon={"https://static.stakestone.io/assets/imgs/tokens/usd1.svg"}
-      />
-    ),
-    value: "USD1",
+    label: <TokenLabel symbol={'USD1'} icon={'https://static.stakestone.io/assets/imgs/tokens/usd1.svg'} />,
+    value: 'USD1',
   },
 ];
