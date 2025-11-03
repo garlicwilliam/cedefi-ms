4c20daf67c57ffbad8c26f175d7c193add569079
william <garlic.william@gmail.com>
2025-11-04 01:33:42 +0800

    update

--- src/pages/request-orders/order-list.tsx
+++ src/pages/request-orders/order-list.tsx
@@ -17,7 +17,7 @@ export const RequestOrderList = () => {
     sorters: {
       permanent: [
         {
-          field: 'id',
+          field: 'requestedAt',
           order: 'desc',
         },
       ],
