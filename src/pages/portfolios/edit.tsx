958f01a7fefef89015846f89d8223398eec720ec
william <garlic.william@gmail.com>
2025-11-06 22:11:38 +0800

    update

--- src/pages/portfolios/edit.tsx
+++ src/pages/portfolios/edit.tsx
@@ -1,11 +1,13 @@
-import { Edit, TextField, useForm } from "@refinedev/antd";
-import { Form, Select, Typography } from "antd";
-import React from "react";
-import { useTeamMap } from "../../hooks/useTeamMap.tsx";
-import { useUpdate } from "@refinedev/core";
+import { Edit, TextField, useForm } from '@refinedev/antd';
+import { Form, Select, Typography } from 'antd';
+import React, { useCallback } from 'react';
+import { useTeamMap } from '../../hooks/useTeamMap.tsx';
+import { useNavigation, useUpdate } from '@refinedev/core';
+import { useCall } from 'wagmi';
 const { Title } = Typography;
 
 export const PortfolioEdit = () => {
+  const { list } = useNavigation();
   const { formProps, query, saveButtonProps } = useForm({});
   const record = query?.data?.data;
   const { arr } = useTeamMap();
@@ -14,43 +16,44 @@ export const PortfolioEdit = () => {
   });
 
   const { mutate } = useUpdate({
-    resource: "portfolios",
+    resource: 'portfolios',
     id: record?.id,
     meta: {
-      subPath: "team",
-      method: "patch",
+      subPath: 'team',
+      method: 'patch',
     },
     mutationOptions: {},
   });
 
   const onChangeTeam = (id: string | number) => {
-    console.log("id =", id);
+    console.log('id =', id);
   };
+
+  const onSubmit = useCallback(
+    (values: any) => {
+      mutate({ values }, { onSuccess: () => list('portfolios') });
+    },
+    [mutate, list],
+  );
 
   return (
     <Edit saveButtonProps={saveButtonProps}>
-      <Form
-        {...formProps}
-        layout="vertical"
-        onFinish={async (values) => {
-          mutate({ values: values });
-        }}
-      >
-        <Title level={5}>{"ID"}</Title>
+      <Form {...formProps} layout="vertical" onFinish={onSubmit}>
+        <Title level={5}>{'ID'}</Title>
         <TextField value={record?.id} />
 
-        <Title level={5}>{"投资组合"}</Title>
+        <Title level={5}>{'投资组合'}</Title>
         <TextField value={record?.fundName} />
 
-        <Title level={5}>{"子账号"}</Title>
+        <Title level={5}>{'子账号'}</Title>
         <TextField value={record?.accountName} />
 
-        <Title level={5}>{"Ceffu Wallet"}</Title>
+        <Title level={5}>{'Ceffu Wallet'}</Title>
         <TextField value={record?.ceffuWalletName} />
 
-        <Title level={5}>{"所属团队"}</Title>
+        <Title level={5}>{'所属团队'}</Title>
         <Form.Item
-          name={"teamId"}
+          name={'teamId'}
           rules={[
             {
               required: true,
