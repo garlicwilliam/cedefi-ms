c64b6853011b788b7ace972b8f7e49dc050f14c1
william <garlic.william@gmail.com>
2026-04-18 02:38:06 +0800

    update

--- src/pages/profit-reallocations/list.tsx
+++ src/pages/profit-reallocations/list.tsx
@@ -1,71 +1,71 @@
-import { List, useTable } from "@refinedev/antd";
-import { useTeamMap } from "../../hooks/useTeamMap.tsx";
-import { Table } from "antd";
-import { FilterTypes, getFromType } from "./util.ts";
-import { formatDatetime } from "../../util/time.ts";
-import { usePortfolios } from "../../hooks/usePortfolios.tsx";
-import { SnapshotAtFilter } from "../../components/dropdown/SnapshotAtFilter.tsx";
-import React from "react";
-import { useUsers } from "../../hooks/useUsers.tsx";
+import { List, useTable } from '@refinedev/antd';
+import { useTeamMap } from '../../hooks/useTeamMap.tsx';
+import { Table } from 'antd';
+import { FilterTypes, getFromType } from './util.ts';
+import { formatDatetime } from '../../util/time.ts';
+import { usePortfolios } from '../../hooks/usePortfolios.tsx';
+import { SnapshotAtFilter } from '../../components/dropdown/SnapshotAtFilter.tsx';
+import React from 'react';
+import { useUsers } from '../../hooks/useUsers.tsx';
+import { useOperators } from '../../hooks/useOperators.tsx';
 
 export const ProfitReallocationList = () => {
   const { tableProps, filters, setFilters } = useTable({
-    resource: "profit_reallocations",
+    resource: 'profit_reallocations',
   });
   const { map: teamMap } = useTeamMap();
   const { map: portfolioMap } = usePortfolios();
-  const { map: usersMap } = useUsers();
+  const { map: opMap } = useOperators();
 
   return (
     <List canCreate={true}>
       <Table {...tableProps}>
-        <Table.Column dataIndex="id" title={"ID"} />
+        <Table.Column dataIndex="id" title={'ID'} />
         <Table.Column
           dataIndex="from"
-          title={"From"}
+          title={'From'}
           render={(from, row) => {
-            return getFromType(
-              from,
-              row.fromPortfolioId,
-              teamMap,
-              portfolioMap,
-            );
+            return getFromType(from, row.fromPortfolioId, teamMap, portfolioMap);
           }}
           filters={FilterTypes}
         />
         <Table.Column
           dataIndex="to"
-          title={"To"}
+          title={'To'}
           render={(to, row) => {
-            return getFromType(to, row.toPortfolioId, teamMap, portfolioMap);
+            return (
+              <div style={{ maxWidth: '150px' }}>
+                {getFromType(to, row.toPortfolioId, teamMap, portfolioMap)}
+              </div>
+            );
           }}
           filters={FilterTypes}
         />
-        <Table.Column dataIndex="usdValue" title={"金额(USD)"} />
+        <Table.Column dataIndex="usdValue" title={'金额(USD)'} />
         <Table.Column
           dataIndex="createdAt"
-          title={"时间"}
+          title={'时间'}
           render={(at) => {
             return formatDatetime(at);
           }}
           filterDropdown={() => {
-            return (
-              <SnapshotAtFilter
-                fieldName={"createdAt"}
-                filters={filters}
-                setFilters={setFilters}
-              />
-            );
+            return <SnapshotAtFilter fieldName={'createdAt'} filters={filters} setFilters={setFilters} />;
+          }}
+        />
+        <Table.Column
+          dataIndex="reason"
+          title={'原因'}
+          render={(reason) => {
+            return <div style={{ maxWidth: '300px' }}>{reason}</div>;
           }}
         />
         <Table.Column
           dataIndex="createdBy"
-          title={"发起人"}
+          title={'发起人'}
           render={(uid) => {
-            return usersMap.get(uid)?.email || uid;
+            return opMap.get(uid)?.name || uid;
           }}
         />
-        <Table.Column dataIndex="reason" title={"原因"} />
       </Table>
     </List>
   );
--- src/pages/profit-reallocations/util.ts
+++ src/pages/profit-reallocations/util.ts
@@ -1,15 +1,15 @@
-import { Portfolio, Team } from "../../service/types.ts";
+import { Portfolio, Team } from '../../service/types.ts';
 
 export const FromTypes = [
-  { value: "platform", label: "平台" },
-  { value: "user", label: "用户" },
-  { value: "team_portfolio", label: "团队投组" },
+  { value: 'platform', label: '平台' },
+  { value: 'user', label: '用户' },
+  { value: 'team_portfolio', label: '团队投组' },
 ];
 
 export const FilterTypes = [
-  { value: "platform", text: "平台" },
-  { value: "user", text: "用户" },
-  { value: "team_portfolio", text: "团队投组" },
+  { value: 'platform', text: '平台' },
+  { value: 'user', text: '用户' },
+  { value: 'team_portfolio', text: '团队投组' },
 ];
 
 export function getFromType(
@@ -17,10 +17,10 @@ export function getFromType(
   portfolioId: number | null,
   teamMap: Map<number, Team>,
   portfolioMap: Map<number, Portfolio>,
-): string | undefined {
+): string | undefined | React.ReactNode {
   const fType = FromTypes.find((one) => one.value == type)?.label;
 
-  if (type == "team_portfolio" && portfolioId) {
+  if (type == 'team_portfolio' && portfolioId) {
     const portfolio = portfolioMap.get(portfolioId);
     const teamId = portfolio?.teamId;
     const team = teamId ? teamMap.get(teamId) : undefined;
